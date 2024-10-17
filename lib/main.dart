d1191f6fb780f2ce600e202b55b2c2dd9cb2c57c
Pritam Bhure <pritambhure558@gmail.com>
2024-10-17 19:48:04 +0530

    integrate Firebase using CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:login/SignUpScreen/Signup_welcomeScreen.dart';
+import 'package:provider/provider.dart';
 
+import 'Backend_Logic_Component/Login_Screen_Provider.dart';
+import 'LoginScreen/loginscreen.dart';
 import 'OnboardingScreen/onboarding_screen.dart';
+import 'SignUpScreen/CompletionScreen.dart';
 import 'SignUpScreen/Farm_Info.dart';
 import 'SignUpScreen/Verification_Screen.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -26,7 +44,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  Onbording(),
+      home:  SuccessScreen(),
     );
   }
 }
